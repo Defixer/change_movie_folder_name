75c9d198fbb063cc191675a583cf4c53561a9821
Defixer <jp.mendoza017@gmail.com>
2018-10-29 08:50:05 +0800

    Replace os.rename to shutil.move

--- change_movie_folder_name.py
+++ change_movie_folder_name.py
@@ -2,6 +2,7 @@ import os
 import sys
 import tkinter as tk
 from tkinter import filedialog
+import shutil
 
 application_window = tk.Tk()
 directory = filedialog.askdirectory(parent=application_window,
@@ -16,4 +17,4 @@ for file in files:
 			print("Old File: {}".format(old_file))
 			new_file = os.path.join(directory, file.split("[")[0].rstrip())
 			print("New File: {}".format(new_file))
-			os.rename(old_file, new_file)
+			shutil.move(old_file, new_file)
